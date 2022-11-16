cf4d0308b18a63faf763f1e4df3fb1f112ddc43f
Iván <bevaquaivan@gmail.com>
2022-11-16 14:31:08 -0300

    Se arreglaron errores

--- src/app/service/educacion.service.ts
+++ src/app/service/educacion.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Educacion } from '../model/educacion';
+
 
 @Injectable({
   providedIn: 'root'
